b91167af1022f2e5d4255eba63a28890c2ea8e82
Daniel Imms <tyriar@tyriar.com>
2016-04-02 21:22:45 -0700

    Implement gnome sort
    Fixes #12

---
+++ src/com/growingwiththeweb/sorting/GnomeSort.java
@@ -0,0 +1,21 @@
+package com.growingwiththeweb.sorting;
+
+public class GnomeSort {
+    public static <T extends Comparable<T>> void sort(T[] array) {
+        int i = 0;
+        while (i < array.length) {
+            if (i == 0 || array[i - 1].compareTo(array[i]) <= 0) {
+                i++;
+            } else {
+                swap(array, i, i - 1);
+                i--;
+            }
+        }
+    }
+
+    private static <T extends Comparable<T>> void swap(T[] array, int a, int b) {
+        T temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+}
---
+++ test/com/growingwiththeweb/sorting/GnomeSortTest.java
@@ -0,0 +1,7 @@
+package com.growingwiththeweb.sorting;
+
+public class GnomeSortTest extends BaseSortTest {
+    protected void sort(Integer[] array) {
+        GnomeSort.sort(array);
+    }
+}
