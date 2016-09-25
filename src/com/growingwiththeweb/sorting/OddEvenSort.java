287c84ba14e54acb58e8c3cc993c4c92bfd81f85
Daniel Imms <daimms@microsoft.com>
2016-09-24 23:35:07 -0700

    Add odd even sort

---
+++ src/com/growingwiththeweb/sorting/OddEvenSort.java
@@ -0,0 +1,31 @@
+package com.growingwiththeweb.sorting;
+
+public class OddEvenSort {
+    public static <T extends Comparable<T>> void sort(T[] array) {
+        boolean sorted = false;
+        while (!sorted) {
+            sorted = innerSort(array, 1);
+            sorted = sorted && innerSort(array, 0);
+        }
+    }
+
+    private static <T extends Comparable<T>> boolean innerSort(T[] array, Integer i) {
+        boolean sorted = true;
+        for (; i < array.length - 1; i += 2)
+        {
+            if (array[i].compareTo(array[i + 1]) > 0)
+            {
+                swap(array, i, i + 1);
+                sorted = false;
+            }
+        }
+        return sorted;
+    }
+
+    private static <T extends Comparable<T>> void swap(
+            T[] array, int a, int b) {
+        T temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+}
---
+++ test/com/growingwiththeweb/sorting/OddEvenSortTest.java
@@ -0,0 +1,7 @@
+package com.growingwiththeweb.sorting;
+
+public class OddEvenSortTest extends BaseSortTest {
+    protected void sort(Integer[] array) {
+        OddEvenSort.sort(array);
+    }
+}
