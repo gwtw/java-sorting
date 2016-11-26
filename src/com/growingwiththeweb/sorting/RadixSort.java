57f50086caa990d60ce4c4e215148f0403c1f792
Daniel Imms <daimms@microsoft.com>
2016-11-26 07:59:54 -0800

    Implement radix sort

---
+++ src/com/growingwiththeweb/sorting/RadixSort.java
@@ -0,0 +1,66 @@
+package com.growingwiththeweb.sorting;
+
+public class RadixSort {
+    public static void sort(Integer[] array) {
+        RadixSort.sort(array, 10);
+    }
+
+    public static void sort(Integer[] array, int radix) {
+        if (array.length == 0) {
+            return;
+        }
+
+        // Determine minimum and maximum values
+        int minValue = array[0];
+        int maxValue = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < minValue) {
+                minValue = array[i];
+            } else if (array[i] > maxValue) {
+                maxValue = array[i];
+            }
+        }
+
+        // Perform counting sort on each exponent/digit, starting at the least
+        // significant digit
+        int exponent = 1;
+        while ((maxValue - minValue) / exponent >= 1) {
+            RadixSort.countingSortByDigit(array, radix, exponent, minValue);
+            exponent *= radix;
+        }
+    }
+
+    private static void countingSortByDigit(
+            Integer[] array, int radix, int exponent, int minValue) {
+        int bucketIndex;
+        int[] buckets = new int[radix];
+        int[] output = new int[array.length];
+
+        // Initialize bucket
+        for (int i = 0; i < radix; i++) {
+            buckets[i] = 0;
+        }
+
+        // Count frequencies
+        for (int i = 0; i < array.length; i++) {
+            bucketIndex = (int)(((array[i] - minValue) / exponent) % radix);
+            buckets[bucketIndex]++;
+        }
+
+        // Compute cumulates
+        for (int i = 1; i < radix; i++) {
+            buckets[i] += buckets[i - 1];
+        }
+
+        // Move records
+        for (int i = array.length - 1; i >= 0; i--) {
+            bucketIndex = (int)(((array[i] - minValue) / exponent) % radix);
+            output[--buckets[bucketIndex]] = array[i];
+        }
+
+        // Copy back
+        for (int i = 0; i < array.length; i++) {
+            array[i] = output[i];
+        }
+    }
+}
---
+++ test/com/growingwiththeweb/sorting/RadixSortTest.java
@@ -0,0 +1,7 @@
+package com.growingwiththeweb.sorting;
+
+public class RadixSortTest extends BaseSortTest {
+    protected void sort(Integer[] array) {
+        RadixSort.sort(array);
+    }
+}
